037a234f8c367022b9b71bc410e355f1f7f4d752
Industrylaud <mrfros22@gmail.com>
2020-01-16 23:51:32 +0100

    part 3

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'CoreyMs',
+        'title': 'blog post one',
+        'content': 'first post content',
+        'date_posted': 'january 16, 2020'
+    },
+    {
+        'author': 'Przemsaj',
+        'title': 'blog post two',
+        'content': 'second post content',
+        'date_posted': 'january 16, 2020'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
