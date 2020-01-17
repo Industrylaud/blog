1470d841f8c41330151ea92f39c23889b2b310a4
Industrylaud <mrfros22@gmail.com>
2020-01-17 19:31:01 +0100

    part 5

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'CoreyMs',
-        'title': 'blog post one',
-        'content': 'first post content',
-        'date_posted': 'january 16, 2020'
-    },
-    {
-        'author': 'Przemsaj',
-        'title': 'blog post two',
-        'content': 'second post content',
-        'date_posted': 'january 16, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
